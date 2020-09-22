5af3de2e5f97b42bce6cb6d0328b667f12e5dc75
tvhfrks12678 <tvhfrks12678@gmail.com>
2020-09-22 10:48:07 +0900

    add_hello_controller

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV["API_DOMAIN"] || ""
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
