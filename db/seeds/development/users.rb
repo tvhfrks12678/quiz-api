442af9a83e008feff636c41aa5f7b79f6c6a6c4b
tvhfrks12678 <tvhfrks12678@gmail.com>
2020-09-29 20:27:32 +0900

    add_seed_data_users

---
+++ db/seeds/development/users.rb
@@ -0,0 +1,25 @@
+10.times do |n|
+    name = "user#{n}"
+    email = "#{name}@example.com"
+    user = User.find_or_initialize_by(name: name, email: email, activated: true)
+  
+    if user.new_record?
+        user.password = "password"
+        user.save!
+    end
+end
+  
+puts "users = #{User.count}"
+
+10.times do |n|
+  name = "user#{n}"
+  email = "#{name}@example.com"
+  user = User.find_or_initialize_by(name: name, email: email, activated: true)
+
+  if user.new_record?
+    user.password = "password"
+    user.save?
+  end
+end
+
+puts "users = #{User.count}"
